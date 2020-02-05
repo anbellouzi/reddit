8c385f8d78f69e57b9878ab6eb9499c1c5f0adb2
Anas <anbellouzi@gmail.com>
2020-02-04 22:22:14 -0800

    Users can see Subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -14,6 +14,17 @@ module.exports = (app) => {
 
   })
 
+  // SUBREDDIT
+  app.get("/n/:subreddit", function(req, res) {
+    Post.find({ subreddit: req.params.subreddit })
+    .then(posts => {
+      res.render("posts-index", { posts });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  });
+
   // CREATE
   app.post('/post/new', (req, res) => {
     // INSTANTIATE INSTANCE OF POST MODEL

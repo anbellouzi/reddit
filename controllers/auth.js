d7ba4701fe23476b4300ead95ba23ab345fc04b7
Anas <anbellouzi@gmail.com>
2020-02-26 15:46:35 -0800

    Implemented initial authentication tests

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,21 +12,24 @@ module.exports = (app) => {
   app.post("/sign-up", (req, res) => {
     // Create User and JWT
     const user = new User(req.body);
-    console.log("_____________")
-    console.log("New User created")
-    console.log("_____________")
-    user.save().then((user) => {
-      var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
-      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-      res.redirect('/');
-    });
+  
+    user
+      .save()
+      .then(user => {
+        var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+        res.redirect("/");
+      })
+      .catch(err => {
+        console.log(err.message);
+        return res.status(400).send({ err: err });
+      });
   });
 
   // LOGOUT
  app.get('/logout', (req, res) => {
-   console.log("_____________")
-   console.log("User logged out")
-   console.log("_____________")
+  //  console.log("_____________")
+  //  console.log("User logged out")
+  //  console.log("_____________")
    res.clearCookie('nToken');
    res.redirect('/');
  });

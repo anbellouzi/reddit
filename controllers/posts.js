751ac84cab1ecd1cd0dda9a925ac7f01b3889f56
Anas <anbellouzi@gmail.com>
2020-02-26 15:23:10 -0800

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,53 +4,66 @@ const User = require("../models/user");
 module.exports = (app) => {
 
   app.get("/", (req, res) => {
-    var currentUser = req.user;
-  
-    Post.find({})
-      .then(posts => {
-        res.render("posts-index", { posts, currentUser });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
+   var currentUser = req.user;
+        // res.render('home', {});
+        console.log(req.cookies);
+        Post.find().populate('author')
+        .then(posts => {
+            res.render('posts-index', { posts, currentUser });
+            // res.render('home', {});
+        }).catch(err => {
+            console.log(err.message);
+        })
   });
 
   // SUBREDDIT
   app.get("/n/:subreddit", function(req, res) {
-    Post.find({ subreddit: req.params.subreddit })
-    .then(posts => {
-      res.render("posts-index", { posts });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    var currentUser = req.user;
+    Post.find({ subreddit: req.params.subreddit }).populate('author')
+        .then(posts => {
+            res.render("posts-index", { posts, currentUser });
+        })
+        .catch(err => {
+            console.log(err);
+        });
   });
 
   // CREATE
   app.post("/post/new", (req, res) => {
     if (req.user) {
-      var post = new Post(req.body);
+        var post = new Post(req.body);
+        post.author = req.user._id;
 
-      post.save(function(err, post) {
-        return res.redirect(`/`);
-      });
+        post
+            .save()
+            .then(post => {
+                return User.findById(req.user._id);
+            })
+            .then(user => {
+                user.posts.unshift(post);
+                user.save();
+                // REDIRECT TO THE NEW POST
+                res.redirect(`/posts/${post._id}`);
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
     } else {
-      return res.status(401); // UNAUTHORIZED
+        return res.status(401); // UNAUTHORIZED
     }
-  });
+});
 
   app.get("/posts/:id", function(req, res) {
-    var id = req.params.id
+    var currentUser = req.user;
     // LOOK UP THE POST
-    if (id.match(/^[0-9a-fA-F]{24}$/)) {
 
-      // LOOK UP THE POST
-      Post.findById(req.params.id).populate('comments').then((post) => {
-        res.render('posts-show', { post })
-      }).catch((err) => {
-        console.log(err.message)
-      })
-    }
+    Post.findById(req.params.id).populate('comments').populate('author')
+        .then(post => {
+            res.render("posts-show", { post, currentUser });  
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
   });
 
   // SIGN UP POST
